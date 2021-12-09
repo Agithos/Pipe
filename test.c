9dafc2d3a52e02c2c522ab1be0af43e2f1a2df73
Agithos <ejagathos@gmail.com>
2021-12-10 01:12:04 +0200

    Kwdikas xwris validation kai rea_line

--- test.c
+++ test.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 //sed s/str1/str2/g
 // infile -> sed
@@ -14,13 +16,15 @@ int main(void)
     int c_pid;                  // child pid
     int fd_file;
     int char_read;
-    char* buffer[128];
+    char buffer[128];
+    char finalPrint[256];
     memset(buffer, 0 , sizeof(buffer));
+    memset(finalPrint,0,sizeof(finalPrint));
 
 
     char* string1 = "ijkl";             //argv[1];
     char* string2 = "01af";             //argv[2];
-    char* pathFile = "./infile.txt"     //argv[3];
+    char* pathFile = "./infile.txt";     //argv[3];
 
     pipe(pipePtC);
     pipe(pipeCtP);
@@ -32,19 +36,25 @@ int main(void)
         close(pipePtC[0]);
         close(pipeCtP[1]);
 
-        fd_file = open(pathFile, O_RDONLY);     // anoigw arxeio (infile)
+        if ( (fd_file = open(pathFile, O_RDONLY)) < 0)  // anoigw arxeio (infile)
+        {
+            perror("Error reading\n");
+        }   
 
         while( (char_read = read(fd_file, buffer,sizeof(buffer)-1)) > 0)        // i thelei fd me th mia to arxeio
         {
             write(pipePtC[1], buffer, char_read);   // grafw sto input
             memset(buffer, 0, sizeof(buffer));
         } 
-        close(pipePtc[1]);
+        close(pipePtC[1]);
+        wait(0);
 
         while( (char_read = read(pipeCtP[0], buffer, sizeof(buffer)-1)) > 0)
         {
-            printf("Edw: %s",char_read);
+            sprintf(finalPrint, "Data received through pipe %s",buffer);
+            printf("%s",finalPrint);
         }
+     
         close(pipeCtP[0]);
     }
     else
@@ -53,61 +63,15 @@ int main(void)
         close(pipeCtP[0]);
         close(pipePtC[1]);
 
-        char* command = sprintf("sed s/",string1,"/",string2,"/g");  // command init ?
+        //char* command = sprintf("s/",string1,"/",string2,"/g");  // command init ?
         dup2(pipePtC[0],0);
         dup2(pipeCtP[1],1);
+
         close(pipeCtP[1]);
         close(pipePtC[1]);
-        execl("usr/bin/sed", "sed", "s/ijklmno/010101/g", "./infile.txt",(char*)0);
+
+        execl("/usr/bin/sed", "sed", "s/ijklmno/010101/g", "./infile.txt",(char*)0);
     }
 
     return 0;
 }
-
-
-
-
-
-
-// char* path_file = "./infile.txt";
-// char* str1 = "ijklmno";
-// char* str2 = "01010101";
-
-// int main()
-// {
-//     
-//    
-
-//     int fd[2];
-//     int c_fd;
-//     int fd_file;
-//     pipe(fd);
-//     fd_file = open(path_file);
-//     dup2(fd_file,fd[0]);
-
-//     c_fd = fork();
-//     if(c_fd == 0)
-//     {
-//         close(fd[0]);
-//         sprintf()
-//         dup()
-//         system()
-//     }
-//     else
-//     {
-//         close(fd[1]);
-//     }
-    
-//     //dup infile me fd[0]
-// }
-
-
-
-// sed s/
-
-/*
-void test1()
-{
-    printf("Test1");
-}
-*/
